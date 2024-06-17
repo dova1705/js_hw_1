d4da130d20bae4d294ae012d37095169df479b9e
Dovlet <allakov.dovlet@gmail.com>
2024-06-17 22:16:08 +0300

    js_hw_1

---
+++ script.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        title: 'Квадрат №1'
+    },
+    {
+        title: 'Квадрат №2'
+    },
+    {
+        title: 'Квадрат №3'
+    },
+    {
+        title: 'Квадрат №4'
+    }
+];
+
+const container = document.querySelector('.container');
+
+data.forEach(element => {
+    const square = document.createElement('div'); //создание нового элемента div
+    square.classList = 'square'; //добавление нового класса
+    square.innerText = element.title; //добавление текста внутрь элемента div
+    container.append(square);
+
+    // Добавление обработчика события клика для каждого квадрата
+    // square.onClick = function() {
+    //     square.classList.toggle('active'); // переключение класса active для квадрата
+    // }
+    square.addEventListener('click', function(){ 
+        square.classList.toggle('active');
+    })
+});
